8d8613de918c820cbf7cb19e103debe445d006ce
MIHAI.IACHIMOVSCHI <MIHAI.IACHIMOVSCHI@LABINFO20>
2023-05-18 15:28:46 +0200

    Aggiungere i file di progetto.

---
+++ Gestione_conto_corente.cpp
@@ -0,0 +1,12 @@
+// Gestione_conto_corente.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
